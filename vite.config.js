acb2c0028d6852a843fc6cd477c1de458ca2f973
Atilola Victor <atilolavictor80@gmail.com>
2024-07-05 01:41:06 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Edustage-ReactJs/', // Adjust this to match your GitHub Pages repository name
+  build: {
+    outDir: 'dist', // Set the output directory for the build
+    emptyOutDir: true, // Ensure the output directory is emptied before building
+  },
 });
